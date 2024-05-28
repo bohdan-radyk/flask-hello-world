1e97de82b27e627dbff79adc08cee5c8f63b6604
marian-seliuchenko <you@example.com>
2024-05-28 23:06:11 +0300

    Assinged New Model

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ safety_settings = [
   },
 ]
 
-model = genai.GenerativeModel(model_name="tunedModels/qs-support-cdkuvhslbdjf",
+model = genai.GenerativeModel(model_name="gemini-1.5-flash",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
